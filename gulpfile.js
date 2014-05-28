f50e5c3c44e0d0dd4f5720eba9042f65242869f5
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-05-28 08:04:49 +0300

    improved gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,42 @@ var gulp = require('gulp'),
   livereload = require('gulp-livereload'),
   gulpif = require('gulp-if');
 
-//Use in command line: NODE_ENV=production gulp js
 var DEVELOPMENT = 'development',
-    PRODUCTION = 'production';
+    PRODUCTION = 'production',
+    BUILD="demo/",
+    ASSETS= "/assets";
 
 var env = process.env.NODE_ENV || DEVELOPMENT;
 if (env!==DEVELOPMENT) env = PRODUCTION;
 
-var buildPath = env;
-    outputDir = "demo/"+buildPath,
-    assets= "/assets";
+function getOutputDir() {
+  return BUILD+env;
+}
 
 gulp.task('jade', function() {
   return gulp.src("src/templates/**/index.jade")
     .pipe(jade())
-    .pipe(gulp.dest(outputDir));
+    .pipe(gulp.dest(getOutputDir()));
 });
 
 gulp.task('js', function() {
   return gulp.src('src/js/main.js')
     .pipe(browserify({ debug: env === DEVELOPMENT }))
     .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulp.dest(outputDir+assets+'/js'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/js'));
 });
 gulp.task('holder', function() {
   return gulp.src('src/js/holder.js')
     .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulp.dest(outputDir+assets+'/js'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/js'));
 });
 gulp.task('fonts', function() {
   return gulp.src('bower_components/bootstrap/fonts')
-    .pipe(gulp.dest(outputDir+assets+'/fonts'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/fonts'));
 });
 gulp.task('fonts', function() {
   return gulp.src('bower_components/bootstrap-sass/fonts/*')
-    .pipe(gulp.dest(outputDir+assets+'/fonts'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/fonts'));
 });
 gulp.task('sass', function() {
   var config = {};
@@ -54,7 +55,7 @@ gulp.task('sass', function() {
 
   return gulp.src('src/sass/main.scss')
     .pipe(sass(config))
-    .pipe(gulp.dest(outputDir+assets+'/css'));
+    .pipe(gulp.dest(getOutputDir()+ASSETS+'/css'));
 });
 
 gulp.task('watch', function() {
@@ -62,10 +63,20 @@ gulp.task('watch', function() {
   gulp.watch('src/js/**/*.js', ['js']);
   gulp.watch('src/sass/**/*.scss', ['sass']);
   var server = livereload();
-  gulp.watch('demo/**').on('change', function(file) {
+  gulp.watch(BUILD+'**').on('change', function(file) {
     server.changed(file.path);
   });
 });
 
 gulp.task('default', ['fonts', 'js', 'holder', 'jade', 'sass']);
 gulp.task('live', ['js', 'holder', 'jade', 'sass', 'watch']);
+
+gulp.task('production', function() {
+  env = PRODUCTION;
+
+  gulp.run('fonts');
+  gulp.run('js');
+  gulp.run('holder');
+  gulp.run('jade');
+  gulp.run('sass');
+});
