6935c5fb6905d2552b87b7596db4cb793a085766
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2014-05-30 17:27:17 +0300

    main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,1 @@
-/*
-var logger = require('./logger');
-var animationEvents = require('animationevents');
-var requestAnimFrame = require("animationframe");
-var $ = require("jquery");
-
-require("bootstrapify");
-require("./responsiveCarousel");
-
-var onEnterFrame = function() {
-  //logger.log(animationEvents.animationEventEnd);
-  requestAnimFrame(onEnterFrame);
-}
-
-requestAnimFrame(onEnterFrame);
-$("h1").html("test");
-*/
-
 require("./responsiveCarousel");
